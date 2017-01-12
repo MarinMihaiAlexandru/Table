ac79f329b3f450f92e386a58514588fda620ed0e
MarinMihaiAlexandru <marin.mihai1997@yahoo.com>
2017-01-12 03:42:21 +0200

    Add files via upload
    updated board

--- main - Copie.cpp
+++ main - Copie.cpp
@@ -3,177 +3,81 @@
 
 using namespace std;
 
-int board[17][17];
-
-const int startup[15][15] =
+struct Triangles{int color;int piecesNumber;}triangle[24];
+int board[15][14];
+const int startup[15][14] =
 {
-    //40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    13,14,15,16,17,18,0,0,0,19,20,21,22,23,24,
-    0,42,43,0,0,0,0,0,0,0,43,0,0,42,0,
-    0,42,43,0,0,0,0,0,0,0,43,0,0,42,0,
-    0,42,43,0,0,0,0,0,0,0,43,0,0,0 ,0,
-    0,42,0 ,0,0,0,0,0,0,0,43,0,0,0, 0,
-    0,42,0 ,0,0,0,0,0,0,0,43,0,0,0, 0,
-      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,43,0 ,0,0,0,0,0,0,0,42,0,0,0 ,0,
-    0,43,0 ,0,0,0,0,0,0,0,42,0,0,0 ,0,
-    0,43,42,0,0,0,0,0,0,0,42,0,0,0 ,0,
-    0,43,42,0,0,0,0,0,0,0,42,0,0,43,0,
-    0,43,42,0,0,0,0,0,0,0,42,0,0,43,0,
-    12,11,10,90,80,70,0,0,0,60,50,40,30,20,10,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    //40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    13,14,15,16,17,18,80,80,19,20,21,22,23,24,
+    42,55,55,57,43,55,0,70,43,55,55,55,55,42,
+    42,55,55,57,43,55,0,70,43,55,55,55,55,42,
+    42,55,55,57,43,55,0,70,43,55,55,55,55,55,
+    42,55,55,57,55,56,0,70,43,55,55,55,55,55,
+    42,55,55,57,55,56,0,70,43,55,55,55,55,55,
+      0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    43,55,55,57,55,56,0,70,42,55,55,55,55,55,
+    43,55,55,57,55,56,0,70,42,55,55,55,55,55,
+    43,55,55,57,42,55,0,70,42,55,55,55,55,55,
+    43,55,55,57,42,55,0,70,42,55,55,55,55,43,
+    43,55,55,57,42,55,0,70,42,55,55,55,55,43,
+    12,11,10,9 ,8 ,7 ,80,80,6 ,5 ,4 ,3 ,2 ,1 ,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 };
-void setup ()
+void setup()
 {
     int i, j;
-    for (i = 0; i < 17; i++)
+    for (i=0;i<15;i++)
     {
-        for (j = 0; j < 17; j++)
+        for (j=0;j<14;j++)
         {
-            board[i][j] = startup[i][j];
+            board[i][j]=startup[i][j];
         }
     }
+    triangle[1].color=2;triangle[1].piecesNumber=2;
+    triangle[6].color=1;triangle[6].piecesNumber=5;
+    triangle[8].color=1;triangle[8].piecesNumber=3;
+    triangle[12].color=2;triangle[12].piecesNumber=5;
+    triangle[13].color=1;triangle[13].piecesNumber=5;
+    triangle[17].color=2;triangle[17].piecesNumber=3;
+    triangle[19].color=2;triangle[19].piecesNumber=5;
+    triangle[24].color=1;triangle[24].piecesNumber=2;
 }
-void printb ()
+void printBoard()
 {
-    int a, b;
-    string piece;
-    for (a=0;a<17;a++)
-    {
-        cout << endl;
-        for (b = 0; b < 17; b++)
-        {
-            switch (board[a][b])
-            {
-            case 0:
-                piece = ' ';
-                break;
-            case 1:
-                piece = '1';
-                break;
-            case 2:
-                piece = '2';
-                break;
-            case 3:
-                piece = '3';
-                break;
-            case 4:
-                piece = '4';
-                break;
-            case 5:
-                piece = '5';
-                break;
-            case 6:
-                piece = '6';
-                break;
-            case 7:
-                piece = '7';
-                break;
-            case 8:
-                piece = '8';
-                break;
-            case 9:
-                piece = '9';
-                break;
-            case 10:
-                piece = "10";
-                break;
-            case 11:
-                piece = "11";
-                break;
-            case 12:
-                piece = "12";
-                break;
-            case 13:
-                piece = "13";
-                break;
-            case 14:
-                piece = "14";
-                break;
-            case 15:
-                piece = "15";
-                break;
-            case 16:
-                piece = "16";
-                break;
-            case 17:
-                piece = "17";
-                break;
-            case 18:
-                piece = "18";
-                break;
-            case 19:
-                piece = "19";
-                break;
-            case 20:
-                piece = "20";
-                break;
-            case 21:
-                piece = "21";
-                break;
-            case 22:
-                piece = "22";
-                break;
-            case 23:
-                piece = "23";
-                break;
-            case 24:
-                piece = "24";
-                break;
-            case 40:
-                piece = '_';
-                break;
-            case 41:
-                piece = '|';
-                break;
-            case 42:
-                piece = 'W';
-                break;
-            case 43:
-                piece = 'B';
-                break;
-            cout <<" "<< piece <<" ";
-            }
-       }
-       cout << endl << endl;
-    }
-}
-int main ()
-{
-    int i, j;
-    /*for (i = 0; i < 17; i++)
-    {
-        for (j = 0; j < 17; j++)
-        {
-            cout<<" "<<startup[i][j]<<" ";
-        }
-        cout<<endl;
-    }*/
+    int i,j;
     for(i=0;i<15;i++)
     {
-        {for(j=0;j<15;j++)
-            if(startup[i][j]==0)
+        {for(j=0;j<14;j++)
+            if(startup[i][j]==80)
                 cout<<" ";
-            else if(startup[i][j]==10)
-                cout<<" 10";
-            else if(startup[i][j]==20)
-                cout<<" 20";
-            else if(startup[i][j]==30)
-                cout<<" 30";
-            else if(startup[i][j]==40)
-                cout<<" 40";
-            else if(startup[i][j]==50)
-                cout<<" 50";
-            else if(startup[i][j]==60)
-                cout<<" 60";
+            else if(startup[i][j]==0)
+                cout<<"   ";
             else if(startup[i][j]==70)
-                cout<<" 70";
-            else if(startup[i][j]==80)
-                cout<<" 80";
-            else if(startup[i][j]==90)
-                cout<<" 90";
+                cout<<" ";
+            else if(startup[i][j]==57)
+                cout<<"    ";
+            else if(startup[i][j]==55)
+                cout<<"   ";
+            else if(startup[i][j]==56)
+                cout<<"  ";
+            else if(startup[i][j]==1)
+                cout<<" 01";
+            else if(startup[i][j]==2)
+                cout<<" 02";
+            else if(startup[i][j]==3)
+                cout<<" 03";
+            else if(startup[i][j]==4)
+                cout<<" 04";
+            else if(startup[i][j]==5)
+                cout<<" 05";
+            else if(startup[i][j]==6)
+                cout<<" 06";
+            else if(startup[i][j]==7)
+                cout<<" 07";
+            else if(startup[i][j]==8)
+                cout<<" 08";
+            else if(startup[i][j]==9)
+                cout<<" 09";
             else if(startup[i][j]==10)
                 cout<<" 10";
             else if(startup[i][j]==11)
@@ -204,21 +108,25 @@ int main ()
                 cout<<" 23";
             else if(startup[i][j]==24)
                 cout<<" 24";
-            /*else if(startup[i][j]==40)
-                cout<<" -";
-            else if(startup[i][j]==41)
-                cout<<" |";*/
             else if(startup[i][j]==42)
-                cout<<" W";
+                {
+                    if(j==0)
+                    cout<<" W";
+                    else cout<<" W";
+                }
             else if(startup[i][j]==43)
-                cout<<" B";
+                {
+                    if(j==0)
+                    cout<<" B";
+                    else cout<<" B";
+                }
         }
         cout<<endl;
     }
-    //setup();printb();
-
-
-
-
+}
+int main ()
+{
+    setup();
+    printBoard();
 
 }
